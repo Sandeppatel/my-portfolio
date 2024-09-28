32e902e4f3945f6e3cdf8ca40d042a7f788c04b9
sandeep patel <sandeeppatel977037@gmail.com>
2024-09-27 19:00:08 -0700

    five

--- script.js
+++ script.js
@@ -1,8 +1,51 @@
-//  stricky navbar
+
+/* ============= menu icon nabvar ============== */
+
+let menuIcon = document.querySelector("#menu-icon") ;
+let navbar = document.querySelector(".navbar") ;
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('x') ;
+  navbar.classList.toggle('active') ;
+}
+
+
+
+
+
+/* ================= scrooll secition==============*/
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+
 window.onscroll = () => {
+          sections.forEach(sec =>{
+            let top = window.screenY;
+            let offset  = sec.offsetTop - 150;
+            let height  = sec.offsetHeight ;
+            let id = sec.getAttribute('id')
+
+            if (top >= offset && top < offset + height) {
+                navlinks.forEach(links => {
+                    links.classList.remove('active') ;
+                    document.querySelector('header nav a[href*=' + id +  ']').classList.add('active');
+                })
+            }
+          })
+    
+
+
+    
+    //  stricky navbar
 let header = document.querySelector('.header');
 header.classList.toggle('sticky' , window.scrollY > 100);
+
+menuIcon.classList.remove('x') ;
+navbar.classList.remove('active') ;
+
 };
+
+
 // ========= swipper js =============
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
